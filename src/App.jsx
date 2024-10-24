01f94693549644cadaf0bd889c41015ff29faeff
Nick Hujarski <nickhujarski@Roberts-13-inch-MacBook-Pro.local>
2024-10-23 21:12:02 -0700

    changed profile img

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='page-layout'>
       <Card
-        profileImg={'neweadshot copy.jpeg'}
+        profileImg={'/static/img/neweadshot copy.jpeg'}
         profileName={'Nickolas Hujarski'}
         profileTitle={'Frontend Engineer'}
         profileBio={
